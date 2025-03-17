1eecec51f2b9fb548c63c3b9206cf5ae2a07c5b9
lucas-hatet <lcs.hatet@gmail.com>
2025-03-17 09:43:23 +0100

    page erreur

--- Vinted/src/router/index.js
+++ Vinted/src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 /* Import des views */
 import HomeView from '../views/HomeView.vue'
+import ErrorView from '../views/ErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,14 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "Error404",
+      component: ErrorView,
+      meta: {
+        requiresAuth: false
+      }
     },
   ],
 })
